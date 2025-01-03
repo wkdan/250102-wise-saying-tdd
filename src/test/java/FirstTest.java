cd764e15bbca7758ff61ded0e5005b65ba955cfe
wkdan <jmy8876@gmail.com>
2025-01-03 12:14:05 +0900

    test: 1단계 테스트 작성

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -35,6 +36,27 @@ public class FirstTest {
         app.run();
 
         assertThat(out.toString()).contains("명언앱을 종료합니다.");
+        // 출력값 체크
+    }
+
+    @Test
+    @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
+    void t4() {
+        // 테스트봇 선입력
+        Scanner sc = new Scanner("종료");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        TestApp app = new TestApp();
+        app.run();
+
+
+
+
+        assertThat(out.toString())
+                .contains("== 명언 앱 ==")
+                .contains("명언앱을 종료합니다.");
         // 출력값 체크
     }
 }
