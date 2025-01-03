25b0611edc2a33926350c91a161d9dd895503cd5
wkdan <jmy8876@gmail.com>
2025-01-03 12:48:19 +0900

    refactor: test5 blue

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -24,7 +24,7 @@ public class FirstTest {
     void t3() {
         // 테스트봇 선입력
 
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
 
         assertThat(out)
                 .contains("명언앱을 종료합니다.");
@@ -34,7 +34,7 @@ public class FirstTest {
     @Test
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
     void t4() {
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
 
         assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
@@ -44,7 +44,11 @@ public class FirstTest {
     @Test
     @DisplayName("등록 - 명언 1개 입력")
     void t5() {
-        String out = TestBot.run("등록\n현재를 사랑하라.\n작자미상\n종료"); //선입력
+        String out = TestBot.run("""
+                등록
+                현재를 사랑하라.
+                작자 미상
+                """); //선입력
 
         assertThat(out)
                 .containsSubsequence("명언 : ", "작가 : ");
--- src/test/java/TestBot.java
+++ src/test/java/TestBot.java
@@ -7,7 +7,7 @@ public class TestBot {
     // static 붙이는 이유 -> 상태(데이터) 없는 객체기 때문(상태 없는 객체는 스태틱 사용해도 됨)
 
     public static String run(String input) {
-        Scanner sc = new Scanner(input + "\n");
+        Scanner sc = new Scanner(input + "종료\n");
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
