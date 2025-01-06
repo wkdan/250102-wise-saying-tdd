ca0629e32e1b5283cc03ac11dfa9a9fab727bc86
wkdan <jmy8876@gmail.com>
2025-01-06 10:16:36 +0900

    refactor: 패키지 구조 변경

---
+++ src/test/java/app/global/CommandTest.java
@@ -0,0 +1,15 @@
+package app.global;
+
+import app.domain.wiseSaying.global.Command;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CommandTest {
+
+    @Test
+    @DisplayName("command Test 최초 테스트")
+    void t1() {
+        Command cmd = new Command();
+    }
+
+}
