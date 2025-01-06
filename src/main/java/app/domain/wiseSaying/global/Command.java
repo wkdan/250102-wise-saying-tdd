4546e23e7aa04d972bc4b464b19b37d4b5c73175
wkdan <jmy8876@gmail.com>
2025-01-06 10:42:56 +0900

    feat: command t5 green

--- src/main/java/app/domain/wiseSaying/global/Command.java
+++ src/main/java/app/domain/wiseSaying/global/Command.java
@@ -3,10 +3,23 @@ package app.domain.wiseSaying.global;
 public class Command {
 
     String actionName;
+    String paramKey;
+    String paramValue;
 
     public Command(String cmd) {
         String[] cmdBits = cmd.split("\\?");
         actionName = cmdBits[0];
+
+        if(cmdBits.length < 2) {
+            paramValue = "";
+            return;
+        }
+
+        String param = cmdBits[1];
+
+        String[] paramBits = param.split("=");
+        paramKey = paramBits[0];
+        paramValue = paramBits[1];
     }
 
     public String getActionName() {
@@ -14,7 +27,8 @@ public class Command {
     }
 
     public int getParam() {
-        return 0;
+
+        return Integer.parseInt(paramValue);
     }
     // 쪼개기 작업
 }
