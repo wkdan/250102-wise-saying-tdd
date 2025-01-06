b3ee3a8b34baa526f7a4258da3818b4663d692bd
wkdan <jmy8876@gmail.com>
2025-01-06 10:25:10 +0900

    test: command t2 red

--- src/main/java/app/domain/wiseSaying/global/Command.java
+++ src/main/java/app/domain/wiseSaying/global/Command.java
@@ -2,5 +2,13 @@ package app.domain.wiseSaying.global;
 
 public class Command {
 
+    public Command(String cmd) {
+
+    }
+
+    public String getActionName() {
+
+        return "삭제";
+    }
     // 쪼개기 작업
 }
