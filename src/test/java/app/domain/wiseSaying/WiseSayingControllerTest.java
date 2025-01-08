929342fd910df184cecc3d908e31ce7c9f71cd8b
wkdan <jmy8876@gmail.com>
2025-01-08 12:28:14 +0900

    test: controller test t14 red

--- src/test/java/app/domain/wiseSaying/WiseSayingControllerTest.java
+++ src/test/java/app/domain/wiseSaying/WiseSayingControllerTest.java
@@ -1,5 +1,6 @@
 package app.domain.wiseSaying;
 
+import app.domain.wiseSaying.repository.WiseSayingFileRepository;
 import app.global.AppConfig;
 import app.standard.TestBot;
 import app.standard.Util;
@@ -212,5 +213,23 @@ public class WiseSayingControllerTest {
 
         assertThat(out)
                 .contains("등록된 명언이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("빌드")
+    void t14() {
+        String out = TestBot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                빌드
+                """);
+
+        boolean rst = Util.File.exists(WiseSayingFileRepository.getBuildPath());
+
+        assertThat(rst).isTrue();
     }
 }
