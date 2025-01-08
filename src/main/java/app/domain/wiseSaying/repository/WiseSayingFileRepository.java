6d91746db54d1817cfec6c03bc085bac378a50d1
wkdan <jmy8876@gmail.com>
2025-01-08 09:49:52 +0900

    test: repository test t5 blue

--- src/main/java/app/domain/wiseSaying/repository/WiseSayingFileRepository.java
+++ src/main/java/app/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -14,11 +14,14 @@ public class WiseSayingFileRepository implements WiseSayingRepository {
     private int lastId;
     public WiseSayingFileRepository() {
         System.out.println("파일 DB 사용");
+        lastIdInit();
+    }
+
+    public void lastIdInit() {
         if(!Util.File.exists(DB_PATH + "lastId.txt")) {
             Util.File.createFile(DB_PATH + "lastId.txt");
         }
     }
-
     public WiseSaying save(WiseSaying wiseSaying) {
 
         if(wiseSaying.isNew()) {
