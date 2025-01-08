2bf1d18d19806865280665f635107060cb42aabf
wkdan <jmy8876@gmail.com>
2025-01-08 14:29:25 +0900

    test: repo t7 red

--- src/test/java/app/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/app/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -145,4 +145,22 @@ public class WiseSayingFileRepositoryTest {
                         """.stripIndent().trim());
 
     }
+
+    @Test
+    @DisplayName("현재 저장된 명언의 개수를 가져오는 count")
+    void t7() {
+        WiseSaying wiseSaying1 = new WiseSaying("aaa", "bbb");
+        wiseSayingRepository.save(wiseSaying1);
+
+        WiseSaying wiseSaying2 = new WiseSaying("ccc", "ddd");
+        wiseSayingRepository.save(wiseSaying2);
+
+        int count = wiseSayingRepository.count();
+
+        String  jsonStr = Util.File.readAsString(WiseSayingFileRepository.getBuildPath());
+
+        assertThat(count)
+                .isEqualTo(2);
+
+    }
 }
