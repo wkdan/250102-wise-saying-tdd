5b28d28b907a168bc44f1cbc07636ce9983c8680
wkdan <jmy8876@gmail.com>
2025-01-15 12:22:17 +0900

    feat: dbTest t2 r

--- src/test/java/app/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
+++ src/test/java/app/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,7 +28,7 @@ public class WiseSayingDbRepositoryTest {
     }
 
     @Test
-    @DisplayName("save 테스트")
+    @DisplayName("save, findByID 테스트")
     void t1() {
         WiseSaying wiseSaying = new WiseSaying("현재를 사랑하라", "작자 미상");
         wiseSaying = wiseSayingDbRepository.save(wiseSaying);
@@ -37,5 +39,18 @@ public class WiseSayingDbRepositoryTest {
 
         assertThat(wiseSaying.getId()).isEqualTo(1);
         assertThat(found).isEqualTo(wiseSaying);
+    }
+
+    @Test
+    @DisplayName("명언 삭제")
+    void t2() {
+
+        WiseSaying wiseSaying = new WiseSaying(1, "aaa", "bbb");
+
+        wiseSaying = wiseSayingDbRepository.save(wiseSaying);
+
+        boolean delRst = wiseSayingDbRepository.deleteById(wiseSaying.getId());
+
+        assertThat(delRst).isTrue();
     }
 }
